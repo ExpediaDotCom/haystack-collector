5a8c52ef3ee78d82bf24261244b611c83ffde71b
krastogi <krastogi@expedia.com>
2019-03-11 13:31:45 +0530

    Passing app_name to terraform as a var

--- deployment/terraform/kinesis-span-collector/variables.tf
+++ deployment/terraform/kinesis-span-collector/variables.tf
@@ -16,6 +16,7 @@ variable "memory_limit"{}
 variable "jvm_memory_limit"{}
 variable "cpu_request"{}
 variable "cpu_limit"{}
+variable "app_name"{ default = "kinesis-span-collector" }
 variable "env_vars" {}
 
 variable "termination_grace_period" {
