e3d0aa3314d3929b78db6dc0b37541d431152c45
absrivastava <absrivastava@expedia.com>
2019-12-16 17:01:36 +0530

    Adding test for spansize validation

--- commons/src/test/scala/com/expedia/www/haystack/collector/commons/unit/ProtoSpanExtractorSpec.scala
+++ commons/src/test/scala/com/expedia/www/haystack/collector/commons/unit/ProtoSpanExtractorSpec.scala
@@ -1,6 +1,6 @@
 package com.expedia.www.haystack.collector.commons.unit
 
-import com.expedia.open.tracing.Span
+import com.expedia.open.tracing.{Span, Tag, TagOrBuilder}
 import com.expedia.www.haystack.collector.commons.ProtoSpanExtractor
 import com.expedia.www.haystack.collector.commons.ProtoSpanExtractor._
 import com.expedia.www.haystack.collector.commons.config.{ExtractorConfiguration, Format, SpanMaxSize, SpanValidation}
@@ -11,6 +11,7 @@ import org.scalatest.{FunSpec, Matchers}
 import org.slf4j.Logger
 
 import scala.collection.immutable.ListMap
+import scala.collection.mutable.ArrayBuffer
 
 class ProtoSpanExtractorSpec extends FunSpec with Matchers with MockitoSugar {
 
@@ -25,26 +26,41 @@ class ProtoSpanExtractorSpec extends FunSpec with Matchers with MockitoSugar {
   private val StartTime = System.currentTimeMillis() * 1000
   private val Duration = 42
   private val Negative = -42
+  private val SampleErrorTag = Tag.newBuilder().setKey("key1").setVBool(true).build()
+  private val SpanSizeLimit = 800
+
+  def createTags(maxTagsLimit : Int): Array[Tag] = {
+    val tags = ArrayBuffer[Tag]()
+    // adding Error Tag by default
+    tags += SampleErrorTag
+    for (i <- 0 until maxTagsLimit){
+     tags += Tag.newBuilder().setKey("key" + i).setVStr("value" + i).build()
+    }
+    tags.toArray
+  }
 
   describe("Protobuf Span Extractor") {
     val mockLogger = mock[Logger]
-    val spanValidationConfig = SpanValidation(SpanMaxSize(enable = false, 5000, "", ""))
 
-    val protoSpanExtractor = new ProtoSpanExtractor(ExtractorConfiguration(Format.PROTO, spanValidationConfig), mockLogger, List())
+    val spanSizeValidationConfig = SpanValidation(SpanMaxSize(enable = true, SpanSizeLimit, "", ""))
+    val protoSpanExtractor = new ProtoSpanExtractor(ExtractorConfiguration(Format.PROTO, spanSizeValidationConfig), mockLogger, List())
 
     val largestInvalidStartTime = SmallestAllowedStartTimeMicros - 1
+
+
     // @formatter:off
-    val nullSpanIdSpan         = createSpan(NullString,  TraceId,     ServiceName1, OperationName1, StartTime,               Duration)
-    val emptySpanIdSpan        = createSpan(EmptyString, TraceId,     ServiceName2, OperationName1, StartTime,               Duration)
-    val nullTraceIdSpan        = createSpan(SpanId,      NullString,  ServiceName1, OperationName1, StartTime,               Duration)
-    val emptyTraceIdSpan       = createSpan(SpanId,      EmptyString, ServiceName2, OperationName1, StartTime,               Duration)
-    val nullServiceNameSpan    = createSpan(SpanId,      TraceId,     NullString,   OperationName1, StartTime,               Duration)
-    val emptyServiceNameSpan   = createSpan(SpanId,      TraceId,     EmptyString,  OperationName2, StartTime,               Duration)
-    val nullOperationNameSpan  = createSpan(SpanId,      TraceId,     ServiceName1, NullString,     StartTime,               Duration)
-    val emptyOperationNameSpan = createSpan(SpanId,      TraceId,     ServiceName2, EmptyString,    StartTime,               Duration)
-    val tooSmallStartTimeSpan  = createSpan(SpanId,      TraceId,     ServiceName1, OperationName1, largestInvalidStartTime, Duration)
-    val negativeStartTimeSpan  = createSpan(SpanId,      TraceId,     ServiceName2, OperationName1, Negative,                Duration)
-    val tooSmallDurationSpan   = createSpan(SpanId,      TraceId,     ServiceName1, OperationName1, StartTime,               Negative)
+    val nullSpanIdSpan         = createSpan(NullString,  TraceId,     ServiceName1, OperationName1, StartTime,               Duration, createTags(1))
+    val emptySpanIdSpan        = createSpan(EmptyString, TraceId,     ServiceName2, OperationName1, StartTime,               Duration, createTags(1))
+    val nullTraceIdSpan        = createSpan(SpanId,      NullString,  ServiceName1, OperationName1, StartTime,               Duration, createTags(1))
+    val emptyTraceIdSpan       = createSpan(SpanId,      EmptyString, ServiceName2, OperationName1, StartTime,               Duration, createTags(1))
+    val nullServiceNameSpan    = createSpan(SpanId,      TraceId,     NullString,   OperationName1, StartTime,               Duration, createTags(1))
+    val emptyServiceNameSpan   = createSpan(SpanId,      TraceId,     EmptyString,  OperationName2, StartTime,               Duration, createTags(1))
+    val nullOperationNameSpan  = createSpan(SpanId,      TraceId,     ServiceName1, NullString,     StartTime,               Duration, createTags(1))
+    val emptyOperationNameSpan = createSpan(SpanId,      TraceId,     ServiceName2, EmptyString,    StartTime,               Duration, createTags(1))
+    val tooSmallStartTimeSpan  = createSpan(SpanId,      TraceId,     ServiceName1, OperationName1, largestInvalidStartTime, Duration, createTags(1))
+    val negativeStartTimeSpan  = createSpan(SpanId,      TraceId,     ServiceName2, OperationName1, Negative,                Duration, createTags(1))
+    val tooSmallDurationSpan   = createSpan(SpanId,      TraceId,     ServiceName1, OperationName1, StartTime,               Negative, createTags(1))
+    val largeSizeSpan          = createSpan(SpanId,      TraceId,     ServiceName1, OperationName1, StartTime,               Duration, createTags(50))
     val spanMap = ListMap(
       "NullSpanId"         -> (nullSpanIdSpan,         SpanIdIsRequired.format(ServiceName1, OperationName1)),
       "EmptySpanId"        -> (emptySpanIdSpan,        SpanIdIsRequired.format(ServiceName2, OperationName1)),
@@ -70,9 +86,14 @@ class ProtoSpanExtractorSpec extends FunSpec with Matchers with MockitoSugar {
       Mockito.verifyNoMoreInteractions(mockLogger)
     }
 
+    it("should truncate tags to reduce span when spansize exceeded") {
+      val kvPairs = protoSpanExtractor.extractKeyValuePairs(largeSizeSpan.toByteArray)
+      kvPairs.foreach(kv => assert(kv.value.length < SpanSizeLimit))
+    }
+
     it("should pass validation if the number of operation names is below the limit") {
       for (i <- 0 to ProtoSpanExtractor.MaximumOperationNameCount) {
-        val span = createSpan(SpanId + i, TraceId + i, ServiceName1, OperationName1 + i, StartTime + i, Duration + i)
+        val span = createSpan(SpanId + i, TraceId + i, ServiceName1, OperationName1 + i, StartTime + i, Duration + i, createTags(1))
         val kvPairs = protoSpanExtractor.extractKeyValuePairs(span.toByteArray)
         kvPairs.size shouldBe 1
       }
@@ -85,7 +106,8 @@ class ProtoSpanExtractorSpec extends FunSpec with Matchers with MockitoSugar {
                          serviceName: String,
                          operationName: String,
                          startTimeMicros: Long,
-                         durationMicros: Long) = {
+                         durationMicros: Long,
+                         tags: Seq[Tag]) = {
     val builder = Span.newBuilder()
     if (spanId != null) {
       builder.setSpanId(spanId)
@@ -98,6 +120,9 @@ class ProtoSpanExtractorSpec extends FunSpec with Matchers with MockitoSugar {
     }
     if (operationName != null) {
       builder.setOperationName(operationName)
+    }
+    if (tags.nonEmpty) {
+      tags.foreach(tag => builder.addTags(tag))
     }
     builder.setStartTime(startTimeMicros)
     builder.setDuration(durationMicros)
